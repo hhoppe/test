2d05ee372d7f32362b480f587996f6d0c12e75f2
Hugues Hoppe <hhoppe@gmail.com>
2023-12-29 10:32:26 -0800

    Also try githubusercontent.com

--- test.py
+++ test.py
@@ -4,14 +4,14 @@
 # %% [markdown]
 # This notebook tests if these markdown images appear when the notebook file is viewed within github.
 #
-# Absolute link back to github:
+# #### Absolute link back to github using `https://github.com/hhoppe/test/raw/main/`
 # <img src="https://github.com/hhoppe/test/raw/main/images/lake.png" width="120"/>
-# <img src="https://github.com/hhoppe/test/raw/main/images/lake.png"/>
 # <img src="https://github.com/hhoppe/test/raw/main/images/lake.png">
 #
-# <p/>
+# #### Absolute link back to github using `https://raw.githubusercontent.com/hhoppe/test/main/`
+# <img src="https://raw.githubusercontent.com/hhoppe/test/main/images/lake.png" width="120"/>
+# <img src="https://raw.githubusercontent.com/hhoppe/test/main/images/lake.png">
 #
-# Local link:
+# #### Local link
 # <img src="images/lake.png" width="120"/>
-# <img src="images/lake.png"/>
 # <img src="images/lake.png">
