8b892baa3d8b1eef825f08a258836877e6efedca
Hugues Hoppe <hhoppe@gmail.com>
2023-12-29 10:27:25 -0800

    First test

---
+++ test.py
@@ -0,0 +1,17 @@
+# %% [markdown]
+# # Test notebook
+
+# %% [markdown]
+# This notebook tests if these markdown images appear when the notebook file is viewed within github.
+#
+# Absolute link back to github:
+# <img src="https://github.com/hhoppe/test/raw/main/images/lake.png" width="120"/>
+# <img src="https://github.com/hhoppe/test/raw/main/images/lake.png"/>
+# <img src="https://github.com/hhoppe/test/raw/main/images/lake.png">
+#
+# <p/>
+#
+# Local link:
+# <img src="images/lake.png" width="120"/>
+# <img src="images/lake.png"/>
+# <img src="images/lake.png">
